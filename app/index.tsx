f458acb44a8873695ef6c86eb6d5c028686a7cc6
Kevin Roan <kevinroan@proton.me>
2024-08-16 22:27:14 +0530

    pre auth setup firebase

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import auth from "@react-native-firebase/auth";
 
 export default function Index() {
+  const user = auth().currentUser;
+  const handleAuth = async () => {
+    console.log("authentication begin");
+  };
   return (
     <View
       style={{
@@ -10,6 +15,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="authenticate" onPress={handleAuth} />
     </View>
   );
 }
